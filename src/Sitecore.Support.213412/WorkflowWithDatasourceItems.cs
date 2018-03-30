818af4a7c4f79b71c4056c0dac495379ea9a7545
AnnaAkimova <aak@sitecore.net>
2018-03-30 10:06:03 +0300

    Fix commit

--- src/Sitecore.Support.213412/WorkflowWithDatasourceItems.cs
+++ src/Sitecore.Support.213412/WorkflowWithDatasourceItems.cs
@@ -1,9 +1,10 @@
 ﻿namespace Sitecore.Support.ExperienceEditor.WebEdit.Commands
 {
   using Sitecore;
+  using Sitecore.Globalization;
   using Sitecore.Data.Items;
   using Sitecore.ExperienceEditor.Utils;
-  using Sitecore.Shell.Framework.Commands;
+  using Sitecore.Layouts;
   using Sitecore.Workflows.Simple;
   using System;
   using System.Collections.Generic;
@@ -18,6 +19,9 @@
       List<Item> itemsToFilter = ItemUtility.GetItemsFromLayoutDefinedDatasources(args.DataItem, Context.Device, args.DataItem.Language);
       itemsToFilter.AddRange(ItemUtility.GetPersonalizationRulesItems(args.DataItem, Context.Device, args.DataItem.Language));
       itemsToFilter.AddRange(ItemUtility.GetTestItems(args.DataItem, Context.Device, args.DataItem.Language));
+      #region Added code
+      itemsToFilter.AddRange(this.GetPageDatasources(args.DataItem, Context.Device, args.DataItem.Language));
+      #endregion
       foreach (Item item in ItemUtility.FilterSameItems(itemsToFilter))
       {
         if (item.Access.CanWrite() && (!item.Locking.IsLocked() || item.Locking.HasLock()))
@@ -26,5 +30,21 @@
         }
       }
     }
+    #region Added code
+    protected List<Item> GetPageDatasources(Item item, DeviceItem deviceItem, Language language = null)
+    {
+      List<Item> list = new List<Item>();
+      foreach (RenderingReference reference in item.Visualization.GetRenderings(deviceItem, true))
+      {
+        Item ds = item.Database.GetItem(reference.Settings.DataSource, language ?? Language.Current);
+        if (ds != null)
+        {
+          list.Add(ds);
+        }
+      }
+      return list;
+    }
+    #endregion
+
   }
 }
