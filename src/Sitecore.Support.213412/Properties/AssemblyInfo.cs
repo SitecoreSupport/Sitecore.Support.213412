4a304eb7e13ac4e98361a82db4f87616f0dfad26
AnnaAkimova <aak@sitecore.net>
2018-03-30 10:03:22 +0300

    Init commit

---
+++ src/Sitecore.Support.213412/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.213412")]
+[assembly: AssemblyProduct("Sitecore.Support.213412")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.213412/WorkflowWithDatasourceItems.cs
@@ -0,0 +1,30 @@
+﻿namespace Sitecore.Support.ExperienceEditor.WebEdit.Commands
+{
+  using Sitecore;
+  using Sitecore.Data.Items;
+  using Sitecore.ExperienceEditor.Utils;
+  using Sitecore.Shell.Framework.Commands;
+  using Sitecore.Workflows.Simple;
+  using System;
+  using System.Collections.Generic;
+
+  [Serializable]
+  public class WorkflowWithDatasourceItems : Sitecore.Shell.Framework.Commands.Workflow
+  {
+    [UsedImplicitly]
+    protected void WorkflowCompleteCallback(WorkflowPipelineArgs args)
+    {
+      base.WorkflowCompleteCallback(args);
+      List<Item> itemsToFilter = ItemUtility.GetItemsFromLayoutDefinedDatasources(args.DataItem, Context.Device, args.DataItem.Language);
+      itemsToFilter.AddRange(ItemUtility.GetPersonalizationRulesItems(args.DataItem, Context.Device, args.DataItem.Language));
+      itemsToFilter.AddRange(ItemUtility.GetTestItems(args.DataItem, Context.Device, args.DataItem.Language));
+      foreach (Item item in ItemUtility.FilterSameItems(itemsToFilter))
+      {
+        if (item.Access.CanWrite() && (!item.Locking.IsLocked() || item.Locking.HasLock()))
+        {
+          WorkflowUtility.ExecuteWorkflowCommandIfAvailable(item, args.CommandItem, args.CommentFields);
+        }
+      }
+    }
+  }
+}
